13dbd8459a531bdbc693c4a90d1c9dfdde05b74d
ERAPARH <panwar.hari69@gmail.com>
2025-09-25 20:20:00 +0530

    Update hello.py

--- hello.py
+++ hello.py
@@ -2,6 +2,7 @@ print("Hello welcome to Jenkins Hariom")
 print("code is run successfully")
 print("Hello from Jenkins! Triggered by Webhook")
 print("Webhook fix test")
+print("hello jenkins show")
 
 
 
