1f0bc5cd82c7184ae0c1ad0f7d5a1b3190e4abd9
ERAPARH <panwar.hari69@gmail.com>
2025-09-25 22:24:57 +0530

    Update hello.py

--- hello.py
+++ hello.py
@@ -5,6 +5,9 @@ print("Webhook fix test")
 print("hello jenkins show")
 name = "hariom"
 print(name)
+# hello.py
+with open("output.txt", "w") as f:
+    f.write("Hello from Python! This is a build artifact.")
 
 
 
