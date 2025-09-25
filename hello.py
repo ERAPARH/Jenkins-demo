28bdcfd5aacfeefb7c1857d60f602b59730b94c7
ERAPARH <panwar.hari69@gmail.com>
2025-09-25 22:53:19 +0530

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,13 +1,5 @@
-print("Hello welcome to Jenkins Hariom")
-print("code is run successfully")
-print("Hello from Jenkins! Triggered by Webhook")
-print("Webhook fix test")
-print("hello jenkins show")
-name = "hariom"
-print(name)
-# hello.py
-with open("output.txt", "w") as f:
-    f.write("Hello from Python! This is a build artifact.")
+def say_hello():
+    return "Hello from Python!"
 
 
 
